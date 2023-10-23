2685ecd825044edf6f2d937164d7c72f0d7a5260
Shoghik <shoghik.kachatryan@dci-student.org>
2023-10-23 18:04:26 +0200

    first commit

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,34 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (BufferedReader buffer = new BufferedReader(new java.io.FileReader(file))) {
+            String current;
+            while ((current = buffer.readLine()) != null) {
+                if (current.startsWith("Name: ")) {
+                    profile.setName(current.substring(6));
+                } else if (current.startsWith("Age: ")) {
+                    profile.setAge(Integer.parseInt(current.substring(5)));
+                } else if (current.startsWith("Email: ")) {
+                    profile.setEmail(current.substring(7));
+                } else if (current.startsWith("Phone: ")) {
+                    profile.setPhone(Long.parseLong(current.substring(7)));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+
+        return profile;
+    }
+
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        fileReader.getDataFromFile(new File("src/main/resources/Profile.txt"));
     }
 }
